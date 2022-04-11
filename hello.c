7d57d9efb3905d96a9de7865d4483266aeaf5641
Jason Evans <jsevans@mailfence.com>
2022-04-11 13:08:55 +0200

    change to podman

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@
 #endif
 
 #ifndef DOCKER_GREETING
-	#define DOCKER_GREETING "Hello from Docker!"
+	#define DOCKER_GREETING "Hello from Podman!"
 #endif
 
 #ifndef DOCKER_ARCH
@@ -18,19 +18,19 @@ const char message[] =
 	DOCKER_GREETING "\n"
 	"This message shows that your installation appears to be working correctly.\n"
 	"\n"
-	"To generate this message, Docker took the following steps:\n"
-	" 1. The Docker client contacted the Docker daemon.\n"
-	" 2. The Docker daemon pulled the \"" DOCKER_IMAGE "\" image from the Docker Hub.\n"
+	"To generate this message, Podman took the following steps:\n"
+	" 1. The Podman client contacted the Podman daemon.\n"
+	" 2. The Podman daemon pulled the \"" DOCKER_IMAGE "\" image from the Podman Hub.\n"
 	"    (" DOCKER_ARCH ")\n"
-	" 3. The Docker daemon created a new container from that image which runs the\n"
+	" 3. The Podman daemon created a new container from that image which runs the\n"
 	"    executable that produces the output you are currently reading.\n"
-	" 4. The Docker daemon streamed that output to the Docker client, which sent it\n"
+	" 4. The Podman daemon streamed that output to the Podman client, which sent it\n"
 	"    to your terminal.\n"
 	"\n"
 	"To try something more ambitious, you can run an Ubuntu container with:\n"
 	" $ docker run -it ubuntu bash\n"
 	"\n"
-	"Share images, automate workflows, and more with a free Docker ID:\n"
+	"Share images, automate workflows, and more with a free Podman ID:\n"
 	" https://hub.docker.com/\n"
 	"\n"
 	"For more examples and ideas, visit:\n"
